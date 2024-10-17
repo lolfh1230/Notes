d3e9d115e0a8c852507e1d222d7c009de143c8b5
lolfh1230 <hraba.cz@seznam.cz>
2024-10-17 13:27:59 +0200

    Add project files.

---
+++ Notes/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Notes
+{
+    partial class NoteTaker
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            titleBox = new TextBox();
+            noteBox = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            previousNotes = new DataGridView();
+            loadButton = new Button();
+            deleteButton = new Button();
+            saveButton = new Button();
+            newButton = new Button();
+            ((System.ComponentModel.ISupportInitialize)previousNotes).BeginInit();
+            SuspendLayout();
+            // 
+            // titleBox
+            // 
+            titleBox.Location = new Point(420, 56);
+            titleBox.Name = "titleBox";
+            titleBox.Size = new Size(537, 23);
+            titleBox.TabIndex = 0;
+            // 
+            // noteBox
+            // 
+            noteBox.Location = new Point(420, 144);
+            noteBox.Multiline = true;
+            noteBox.Name = "noteBox";
+            noteBox.Size = new Size(537, 421);
+            noteBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(420, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(537, 41);
+            label1.TabIndex = 2;
+            label1.Text = "Title:";
+            label1.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            label2.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(420, 82);
+            label2.Name = "label2";
+            label2.Size = new Size(537, 59);
+            label2.TabIndex = 3;
+            label2.Text = "Note:";
+            label2.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // previousNotes
+            // 
+            previousNotes.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            previousNotes.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            previousNotes.Location = new Point(12, 12);
+            previousNotes.Name = "previousNotes";
+            previousNotes.Size = new Size(387, 397);
+            previousNotes.TabIndex = 4;
+            previousNotes.CellContentClick += previousNotes_CellContentClick;
+            // 
+            // loadButton
+            // 
+            loadButton.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            loadButton.Location = new Point(12, 427);
+            loadButton.Name = "loadButton";
+            loadButton.Size = new Size(178, 51);
+            loadButton.TabIndex = 5;
+            loadButton.Text = "Load";
+            loadButton.UseVisualStyleBackColor = true;
+            loadButton.Click += loadButton_Click;
+            // 
+            // deleteButton
+            // 
+            deleteButton.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            deleteButton.Location = new Point(221, 427);
+            deleteButton.Name = "deleteButton";
+            deleteButton.Size = new Size(178, 51);
+            deleteButton.TabIndex = 6;
+            deleteButton.Text = "Delete";
+            deleteButton.UseVisualStyleBackColor = true;
+            deleteButton.Click += deleteButton_Click;
+            // 
+            // saveButton
+            // 
+            saveButton.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            saveButton.Location = new Point(221, 493);
+            saveButton.Name = "saveButton";
+            saveButton.Size = new Size(178, 51);
+            saveButton.TabIndex = 7;
+            saveButton.Text = "Save";
+            saveButton.UseVisualStyleBackColor = true;
+            saveButton.Click += saveButton_Click;
+            // 
+            // newButton
+            // 
+            newButton.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            newButton.Location = new Point(12, 493);
+            newButton.Name = "newButton";
+            newButton.Size = new Size(178, 51);
+            newButton.TabIndex = 8;
+            newButton.Text = "New note";
+            newButton.UseVisualStyleBackColor = true;
+            newButton.Click += newButton_Click;
+            // 
+            // NoteTaker
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(64, 64, 64);
+            ClientSize = new Size(969, 577);
+            Controls.Add(newButton);
+            Controls.Add(saveButton);
+            Controls.Add(deleteButton);
+            Controls.Add(loadButton);
+            Controls.Add(previousNotes);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(noteBox);
+            Controls.Add(titleBox);
+            Name = "NoteTaker";
+            Text = "Note Taker";
+            Load += NoteTaker_Load;
+            ((System.ComponentModel.ISupportInitialize)previousNotes).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox titleBox;
+        private TextBox noteBox;
+        private Label label1;
+        private Label label2;
+        private DataGridView previousNotes;
+        private Button loadButton;
+        private Button deleteButton;
+        private Button saveButton;
+        private Button newButton;
+    }
+}
